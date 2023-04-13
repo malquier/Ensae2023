b99718cb26725d509c66f979902cba65126836ce
malquier <126264068+malquier@users.noreply.github.com>
2023-04-13 12:13:42 +0200

    Update main.py

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -1,8 +1,32 @@
 from graph import Graph, graph_from_file
+from Fileprio import *
+from Union_set import * 
 
+
+## Initialisation:
 
 data_path = "input/"
-file_name = "network.01.in"
+file_name_network = "network.2.in"
+file_name_route = "routes.2.in"
+file_name_truck = "trucks.2.in"
 
-g = graph_from_file(data_path + file_name)
-print(g)
+g2 = graph_from_file(open(data_path + file_name_network,"r").read())
+routes = route_from_file(open(data_path + file_name_route,"r").read())
+trucks = trucks2(open(data_path + file_name_truck,"r").read())
+budget = 10**9
+
+
+## Simplification du graphe par kruskal:
+
+# g = kruskal(g2)
+
+
+## Puissance minimale par kruskal :
+
+# print(puissance_power_kruskal(g2,1,500))
+
+## Knapsack par génération :
+
+# print(knapsack_generative(g2, routes, trucks, budget))
+
+##
