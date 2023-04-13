525aee851c03cf4a74a3b28c3b3f4d2501f93db3
malquier <126264068+malquier@users.noreply.github.com>
2023-04-13 12:21:49 +0200

    Update main.py

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -20,10 +20,28 @@ budget = 10**9
 
 # g = kruskal(g2)
 
-
 ## Puissance minimale par kruskal :
 
 # print(puissance_power_kruskal(g2,1,500))
+
+## Test du temps d'éxécution de puissance_mini_power :
+
+def test(filename_network,filename_route):
+    g = graph_from_file(open(filename_network).read())
+    g,u = kruskal(g)
+    m = matrice(open(filename_route).read())
+    routes0 = []
+    n = len(m)
+    t1 = time.time()
+    for i in range(1,n):
+        if len(m[i][0])>0 and len(m[i][1])>0:
+            src,dest = int((m[i][0])),int((m[i][1]))
+            src,dest = src-1,dest-1
+            routes0.append(Union_set.path(u, src, dest)[0])
+    t2 = time.time()
+    return(t2-t1,routes0)
+  
+  print(test(filename_network, filename_route))
 
 ## Knapsack par génération :
 
